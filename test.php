7b885e50b46c2c1a27014f29d79d943ceb706cc2
mohammad <mohammadmghig@gmial.com>
2019-12-14 15:08:36 +0330

    first commit

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+include "BinaryHeap.php";
+$heap = new BinaryHeap();
+
+for ($i=0; $i <1000 ; $i++) { 
+    $heap->insert(mt_rand(0, 1000));
+}
+
+ 
+$heap->insert(7);
+
+$rand = substr(md5(microtime()),rand(0,26),5);
+
+$passArray = array();
+
+$i=0;
+while (!$heap->isEmpty()) {
+    $passArray[$i] = $heap->extract() .$rand ;
+    $i++;
+}
+
+foreach ($passArray as $pass)
+    echo $pass;
+ 
+
+ 
+
